b595d771d79e86e0c3b067c0dca0a2e9bb1d5c03
john-zhang1 <john-zhang1@hotmail.com>
2024-08-22 22:43:19 -0500

    hide profile, update about page and contact us page

--- src/themes/dspace/app/info/contactus/contactus-form/contactus-form.component.ts
+++ src/themes/dspace/app/info/contactus/contactus-form/contactus-form.component.ts
@@ -5,10 +5,10 @@ import {
 
 @Component({
   selector: 'ds-contactus-form',
-  // templateUrl: './contactus-form.component.html',
-  templateUrl: '../../../../../../app/info/contactus/contactus-form/contactus-form.component.html',
-  // styleUrls: ['./contactus-form.component.scss'],
-  styleUrls: ['../../../../../../app/info/contactus/contactus-form/contactus-form.component.scss'],
+  templateUrl: './contactus-form.component.html',
+  // templateUrl: '../../../../../../app/info/contactus/contactus-form/contactus-form.component.html',
+  styleUrls: ['./contactus-form.component.scss'],
+  // styleUrls: ['../../../../../../app/info/contactus/contactus-form/contactus-form.component.scss'],
 })
 export class ContactusFormComponent extends BaseComponent {
 }
